c4c21e034483a0d8e6a2aaed7ef5c2843be11123
Daniel Martínez <85038070+DanielMart-tech@users.noreply.github.com>
2022-04-07 20:18:13 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,26 @@
+import requests
+
+class APICall:
+    def call(self, method, api_url):
+        if method.lower() == "get":
+            r = requests.get(api_url)
+
+        match r.status_code:
+            case 200 | 201 | 202:
+                return r.json()["value"]
+            case _:
+                raise TypeError("API error")
+
+
+class ChuckNorris(APICall):
+    url = "https://api.chucknorris.io/jokes/random?category="
+    categories = ["animal", "dev"]
+
+    def __init__(self, category):
+        self.category = category
+        if not self.category in self.categories:
+            raise TypeError("category options ['animal', 'dev']")
+
+    def get(self):
+        api_url = f"{self.url}{self.category}"
+        return self.call("GET", api_url)
